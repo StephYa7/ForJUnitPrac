556de18bd16e772fbfedccc82b8d1aa487069b13
StephYa7 <1000p@mail.ru>
2024-01-12 20:17:08 +0700

    added hw 4 task

--- src/test/java/Prac_4/book/BookServiceTest.java
+++ src/test/java/Prac_4/book/BookServiceTest.java
@@ -1,23 +1,43 @@
 package Prac_4.book;
 
 
-import Prac_4.hotel.BookingService;
-import Prac_4.hotel.HotelService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class BookServiceTest {
+    BookRepository bookRepository;
+    BookService bookService;
+    List<Book> allBooks;
+    Book book;
+
+    @BeforeEach
+    void options() {
+        bookRepository = mock(BookRepository.class);
+        bookService = new BookService(bookRepository);
+        book = new Book("1", "title1", "author1");
+        allBooks = List.of(book
+                , new Book("2", "title2", "author2")
+                , new Book("3", "title3", "author3"));
+    }
+
     @Test
-    void bookingServiceTest() {
-        HotelService hotelService = mock(HotelService.class);
-        BookingService bookingService = new BookingService(hotelService);
-        when(hotelService.isRoomAvailable(3)).thenReturn(false);
+    void returnBookByGivenId() {
+        when(bookRepository.findById("1")).thenReturn(book);
+        var result = bookService.findBookById("1");
+        assertEquals(result, book);
+    }
 
-        boolean result = bookingService.bookRoom(3);
-
-        assertFalse(result);
+    @Test
+    void returnListOfAllBooks() {
+        when(bookRepository.findAll()).thenReturn(allBooks);
+        var result = bookService.findAllBooks();
+        assertEquals(result, allBooks);
     }
 }
--- src/test/java/Prac_4/hotel/BookingServiceTest.java
+++ src/test/java/Prac_4/hotel/BookingServiceTest.java
@@ -1,6 +1,21 @@
 package Prac_4.hotel;
 
 
-class BookingServiceTest {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class BookingServiceTest {
+    @Test
+    void bookingServiceTest() {
+        HotelService hotelService = mock(HotelService.class);
+        BookingService bookingService = new BookingService(hotelService);
+        when(hotelService.isRoomAvailable(3)).thenReturn(false);
+
+        boolean result = bookingService.bookRoom(3);
+
+        assertFalse(result);
+    }
 }
